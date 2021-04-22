424cc9cccd5dc6191274ccdf1a8f8a5c5b556ba7
Sancterr <81334236+Sancterr@users.noreply.github.com>
2021-04-21 20:34:46 -0700

    Add files via upload

---
+++ link.js
@@ -0,0 +1,18 @@
+// class Link{
+//     constructor(bodyA1,bodyB1){
+//         var options={
+//             bodyA: bodyA1,
+//             bodyB: bodyB1,
+//             length: 10,
+//             stiffness: 1
+//         }
+//     this.link = Matter.Constraint.create(options)
+//     World.add(world,this.link)
+//     }
+
+//     display(){
+//         var pointA = this.bodyA.position
+//         var pointB = this.bodyB.position
+//         line(pointA.x,pointA.y,pointB.x,pointB.y)
+//     }
+// }
